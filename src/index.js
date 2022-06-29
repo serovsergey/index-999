1a5f52e63251a285ea83b0ec2c75b0badcbe9431
Serov Sergey <serovsergey79@gmail.com>
2022-06-30 00:44:58 +0300

    add: modals

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@
   const refs = {
     menuBtn: document.querySelector("[data-menu-button]"),
     mobileMenu: document.querySelector("[data-menu]"),
+    btnLocations: document.querySelector("[data-btn-locations]"),
+    btnFranchise: document.querySelector("[data-btn-franchise]"),
+    backdrop: document.querySelector("[data-modal]"),
+    btnCloseModal: document.querySelectorAll(".btn-close"),
   };
 
   if (refs.menuBtn)
     refs.menuBtn.addEventListener('click', toggleMenu);
+  if (refs.backdrop && refs.btnCloseModal) {
+    console.log(refs.btnCloseModal);
+    if (refs.btnLocations)
+      refs.btnLocations.addEventListener('click', () => (refs.backdrop.setAttribute('data-modal', 'locations')));
+    if (refs.btnFranchise)
+      refs.btnFranchise.addEventListener('click', () => (refs.backdrop.setAttribute('data-modal', 'franchise')));
+    refs.btnCloseModal.forEach(btn => (btn.addEventListener('click', () => (refs.backdrop.setAttribute('data-modal', '')))));
+  }
+
 
   function toggleMenu() {
     const expanded = refs.menuBtn.getAttribute("aria-expanded") == "true" | false;
